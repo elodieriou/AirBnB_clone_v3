598cfba99e6ee69e8a5ded56d44c2190c0f33118
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-19 13:30:45 +0200

    Edit code_review.py and create an handler error for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module starts a Flask web application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ port = getenv('HBNB_API_PORT', '5000')
 def tear_down(exception):
     """The method remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(exception):
+    """ This method handle error 404 """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
