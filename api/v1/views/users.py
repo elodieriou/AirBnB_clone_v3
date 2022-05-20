4343db51471c178838ca674b5611fa7fa504e0cd
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-20 22:25:59 +0200

    Try to fix bugs

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,11 +6,41 @@ from models.user import User
 from models import storage
 from api.v1.views import app_views
 
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """get user information for all users"""
+    users = []
+    for user in storage.all("User").values():
+        users.append(user.to_dict())
+    return jsonify(users)
 
-@app_views.route('/users', methods=['GET'],
+
+@app_views.route('/users/<string:user_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_user(user_id):
+    """get user information for specified user"""
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    """deletes a user based on its user_id"""
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return (jsonify({}))
+
+
+"""@app_views.route('/users', methods=['GET'],
                  strict_slashes=False)
 def get_users():
-    """Retrieves get method for all users"""
+    ""Retrieves get method for all users""
     list_users = []
     all_a = storage.all(User).values()
     for user in all_a:
@@ -21,7 +51,7 @@ def get_users():
 @app_views.route('/users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def get_user_id(user_id):
-    """Retrieves get method for a user with a given id"""
+    ""Retrieves get method for a user with a given id""
     user = storage.get(User, user_id)
     if user is None:
         return abort(404)
@@ -31,19 +61,19 @@ def get_user_id(user_id):
 @app_views.route('/user/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_user(user_id):
-    """ Method that deletes an user based on its ID. """
+    "" Method that deletes an user based on its ID. ""
     user = storage.get(User, user_id)
     if user is None:
         return abort(404)
     user.delete()
     storage.save()
-    return jsonify({})
+    return jsonify({})"""
 
 
-"""@app_views.route('/users', methods=['POST'],
+@app_views.route('/users', methods=['POST'],
                  strict_slashes=False)
 def post_user():
-    "" Method that create a new user. ""
+    """ Method that create a new user. """
     params = request.get_json()
     if params is None:
         abort(400, "Not a JSON")
@@ -59,7 +89,7 @@ def post_user():
 @app_views.route('users/<user_id>',
                  methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
-    "" Method that update an user. ""
+    """ Method that update an user. """
     user = storage.get(User, user_id)
     if user is None:
         return abort(404)
@@ -70,34 +100,5 @@ def put_user(user_id):
     for k, v in params.items():
         if k not in li:
             setattr(user, k, v)
-    user.save()
-    return jsonify(user.to_dict())
-"""
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-def post_user():
-    """create a new user"""
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'email' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing email'}), 400)
-    if 'password' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing password'}), 400)
-    user = User(**request.get_json())
-    user.save()
-    return make_response(jsonify(user.to_dict()), 201)
-
-
-@app_views.route('/users/<string:user_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_user(user_id):
-    """update a user"""
-    user = storage.get("User", user_id)
-    if user is None:
-        abort(404)
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for attr, val in request.get_json().items():
-        if attr not in ['id', 'email', 'created_at', 'updated_at']:
-            setattr(user, attr, val)
     user.save()
     return jsonify(user.to_dict())
