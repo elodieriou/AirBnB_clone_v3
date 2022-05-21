9a723f1816734de512aeb76077a4291551deb89c
Rémi Marçais <remi.marcais87@gmail.com>
2022-05-21 17:11:16 +0200

    Change the statement position

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,8 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 cors = CORS(app, ressources={r'/*': {'origins': '0.0.0.0'}})
+app.register_blueprint(app_views)
 
 host = getenv('HBNB_API_HOST', '0.0.0.0')
 port = getenv('HBNB_API_PORT', '5000')
